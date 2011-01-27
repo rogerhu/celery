f75abe88caa0c4dfb9fa2874152ef1ac6798459c
Ask Solem <ask@celeryproject.org>
2011-01-27 14:50:46 +0100

    Remove Python :: 2.4 from trove classifiers

--- setup.py
+++ setup.py
@@ -112,7 +112,6 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.4",
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
